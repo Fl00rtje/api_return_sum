a363d114b7a64e22cabb0b75e26f0a4720d74af6
Floor Harmsen <floor.harmsen@gmail.com>
2021-07-20 20:23:59 +0200

    added addition in view and only json possible now

--- api_assignment/calculate/urls.py
+++ api_assignment/calculate/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from calculate import views
 
 urlpatterns = [
+    path('sum/', views.addition),
     path('numbers/', views.number_list),  # for testing the basic setup
 ]
--- api_assignment/calculate/views.py
+++ api_assignment/calculate/views.py
@@ -1,11 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import NumberSerializer
 from .models import Number
 
 
 @api_view(['GET'])
-def number_list(request, format=None):
+def number_list(request):
     """
         List all numbers as a test to see if this works.
     """
@@ -13,3 +14,39 @@ def number_list(request, format=None):
         sets = Number.objects.all()
         serializer = NumberSerializer(sets, many=True)
         return Response(serializer.data)
+
+
+@api_view(['POST'])
+def addition(request):
+    """
+    For two given numbers, this function returns the sum.
+    Input: {"input": "100 + 4"}
+    """
+    if request.method == 'POST':
+        try:
+            request_data = request.data["input"]
+            request_data = request_data.replace(" ", "").split("+")
+            request_data = [int(x) for x in request_data]
+            data = {"num_1": request_data[0],
+                    "num_2": request_data[1],
+                    "total": sum(request_data)}
+        except ValueError:
+            request_example = {"input": "100 + 4"}
+            content = {'Error message': f"Only provide integers in the string, for example: {request_example}"}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+        except:
+            request_example = {"input": "100 + 4"}
+            content = {'Error message': f"Something unexpected went wrong. "
+                                        f"Please submit your request in this form: {request_example}"}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+
+    serializer = NumberSerializer(data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+        print(serializer.data)
+        response_data = f"The sum of {serializer.data['num_1']} and {serializer.data['num_2']} is: " \
+                        f"{serializer.data['total']}."
+        return Response(response_data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
