212d71d5a524bc73540af17a5a164f157aa9dfd0
Floor Harmsen <floor.harmsen@gmail.com>
2021-07-20 19:45:12 +0200

    added Number model

--- api_assignment/calculate/models.py
+++ api_assignment/calculate/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+
+class Number(models.Model):
+    num_1 = models.IntegerField(verbose_name="Number 1")
+    num_2 = models.IntegerField(verbose_name="Number 2")
+    total = models.IntegerField(verbose_name="Total of number 1 and number 2", null=True)
+
+    def __str__(self):
+        return f"Number 1: {self.num_1} \n" \
+               f"Number 2: {self.num_2} \n" \
+               f"Total: {self.total}"
