08c542d84324d51aa08eb07817f16d7e5c12ed28
Floor Harmsen <floor.harmsen@gmail.com>
2021-07-22 11:28:16 +0200

    added tests for the addition view

--- api_assignment/calculate/tests/test_views.py
+++ api_assignment/calculate/tests/test_views.py
@@ -1,5 +1,31 @@
-from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIRequestFactory, APITestCase
+from ..views import addition
 
 
-factory = APIRequestFactory()
-request = factory.post('/sum/', {"input": "100 + 4"}, format='json')
+class NumberTestCase(APITestCase):
+    def test_response_status_code_1(self):
+        """
+            Tests the status code with correct input.
+        """
+        factory = APIRequestFactory()
+        request = factory.post('/sum/', {"input": "100 + 4"}, format='json')
+        response = addition(request)
+        self.assertEqual(response.status_code, 201)
+
+    def test_response_status_code_2(self):
+        """
+            Tests the status code with incorrect input ("-" instead of "+").
+        """
+        factory = APIRequestFactory()
+        request = factory.post('/sum/', {"input": "100 - 4"}, format='json')
+        response = addition(request)
+        self.assertEqual(response.status_code, 400)
+
+    def test_response_status_code_3(self):
+        """
+            Tests the status code with incorrect input (added an "a" to the data).
+        """
+        factory = APIRequestFactory()
+        request = factory.post('/sum/', {"input": "100 + 4a"}, format='json')
+        response = addition(request)
+        self.assertEqual(response.status_code, 400)
