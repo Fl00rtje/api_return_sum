53763c59b80c9e118f9b6d0bd470444a0578167f
Floor Harmsen <floor.harmsen@gmail.com>
2021-07-22 09:41:01 +0200

    made request_example DRY

--- api_assignment/calculate/views.py
+++ api_assignment/calculate/views.py
@@ -26,6 +26,7 @@ def addition(request):
     if request.method == 'GET':
         return Response("Please provide input like explained above.")
     else:
+        request_example = {"input": "100 + 4"}
         try:
             request_data = request.data["input"]
             request_data = request_data.replace(" ", "").split("+")
@@ -34,11 +35,9 @@ def addition(request):
                     "num_2": request_data[1],
                     "total": sum(request_data)}
         except ValueError:
-            request_example = {"input": "100 + 4"}
             content = {'Error message': f"Only provide integers in the string, for example: {request_example}"}
             return Response(content, status=status.HTTP_400_BAD_REQUEST)
         except:
-            request_example = {"input": "100 + 4"}
             content = {'Error message': f"Something unexpected went wrong. "
                                         f"Please submit your request in this form: {request_example}"}
             return Response(content, status=status.HTTP_400_BAD_REQUEST)
